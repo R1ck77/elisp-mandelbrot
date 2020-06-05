fffab78f380dcb23315d234c147e3af9a8442e62
Riccardo Di Meo <riccardo.dimeo42@gmail.com>
2020-06-06 00:51:51 +0200

    Fix some read-only mode related issues.
    Also:
    
    - minor refactoring
    - add a stub for a new command (print current location)

--- mandelbrot.el
+++ mandelbrot.el
@@ -9,24 +9,31 @@
   "Keymap for Mandelbrot mode")
 (when (not mandelbrot-mode-map)
   (setq mandelbrot-mode-map (make-keymap))
-  (define-key mandelbrot-mode-map (kbd "r") #'mandelbrot/redraw))
+  (define-key mandelbrot-mode-map (kbd "r") #'mandelbrot/redraw)
+  (define-key mandelbrot-mode-map (kbd "p") #'mandelbrot/print-coordinate))
+
+(defmacro mandelbrot/with-read-only-disabled (&rest forms)
+  `(let ((buffer-read-only nil))
+     ,@forms))
 
 (defun mandelbrot/configure-canvas ()
   (let ((buffer (get-buffer-create mandelbrot-buffer-name)))
     (switch-to-buffer buffer)
-    (setq buffer-read-only nil)
-    (erase-buffer)
+    (mandelbrot/with-read-only-disabled 
+     (erase-buffer))
     buffer))
 
 (defun mandelbrot/redraw ()
   (interactive)
   "Redraw mandelbrot in the current buffer"
-  (message (format "Read only: %s" buffer-read-only))
-  (let ((buffer-read-only nil))
-    (erase-buffer)
-    (message (format "Read only: %s" buffer-read-only))
-    (mandelbrot/draw-4x))
-  (message (format "Read only: %s" buffer-read-only)))
+  (mandelbrot/with-read-only-disabled   
+   (erase-buffer)
+   (mandelbrot/draw-4x)))
+
+(defun mandelbrot/print-coordinate ()
+  (interactive)
+  "Print the coordinate at point"
+  (message "Not yet implemented"))
 
 (defun mandelbrot-mode ()
   "Enter Mandelbrot mode"
@@ -37,6 +44,7 @@
   (setq mode-name "Mbrot")
   (use-local-map mandelbrot-mode-map)
   (run-mode-hooks 'mandelbrot-mode-hook)
+  (setq buffer-read-only t)
   (mandelbrot/redraw))
 
 (provide 'mandelbrot)
